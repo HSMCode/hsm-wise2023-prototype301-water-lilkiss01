db8e89662ff2fd3a87d1b9f4f0c26b12b880ec84
Serap Akbulut <serap.akbulut@students.hs-mainz.de>
2024-01-01 10:12:43 +0100

    Krokodil Game Sound+Effekt+Restart Update

--- Assets/SCRIPTS/SceneRestart.cs
+++ Assets/SCRIPTS/SceneRestart.cs
@@ -1,10 +1,42 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneRestart : MonoBehaviour
+public class GameController : MonoBehaviour
 {
-    public void RestartScene()
+    private bool gameLost = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (gameLost)
+            {
+                RestartGame();
+            }
+            else
+            {
+                
+                MoveCharacterWithSpace();
+            }
+        }
+    }
+
+    
+    public void LoseGame()
+    {
+        gameLost = true;
+        
+    }
+
+    
+    private void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    
+    private void MoveCharacterWithSpace()
+    {
+        
     }
 }
